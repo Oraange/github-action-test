b809ae4de8c71604196e6dbe9f5eb41eb0d817f3
Oraange <hrpp1300@naver.com>
2023-06-10 21:38:55 +0900

    Feat: Add test code in boards

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from .models import Board
+
+class BoardTest(TestCase):
+    def setUp(self):
+        Board.objects.bulk_create(
+            [
+                Board(
+                    title=f"Test title{i+1}",
+                    content="Test content"
+                ) for i in range(5)
+            ]
+        )
+
+    def tearDown(self) -> None:
+        Board.objects.all().delete()
+        return super().tearDown()
+    
+    def test_create_board(self):
+        client = Client()
+        data = {
+            "title": "Test Create title",
+            "content": "Test Create Content"
+        }
+        response = client.post(
+            "/boards", json.dumps(data), content_type="application/json"
+        )
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_board_list(self):
+        client = Client()
+        response = client.get(
+            "/boards"
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_board(self):
+        client = Client()
+        response = client.get(
+            "/boards/1"
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "id": 1,
+            "title": "Test title1",
+            "content": "Test content"
+        })
